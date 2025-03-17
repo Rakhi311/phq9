acb9dd1dfefd3ce8c7fca881db25f6bf2db12120
Rakhi311 <rasah6042@gmail.com>
2025-03-17 15:38:24 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 17 14:51:19 2025
+
+@author: rakhi
+"""
+
+from app import app
+
+if __name__ == "_main_":
+    app.run(debug=False)  # Set debug=False for production
